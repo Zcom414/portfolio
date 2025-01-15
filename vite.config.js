c0a3df9a9b6ca8120447bf7ea3954d8bb2d3bf4a
unknown <jules.cless.cl@gmail.com>
2025-01-15 01:46:43 +0100

    Last commit before 1st push for site

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react()
   ],
-  base: '/portfolio/',
-  build: {
-    chunkSizeWarningLimit: 2000,
-},
-  })
+  base: '/portfolio' 
+})
